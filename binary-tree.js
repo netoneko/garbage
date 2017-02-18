a1dc28ed214c2e951f58a0a21f1d7992f7f3127f
Kirill Maksimov <kirill.maksimov@ironsrc.com>
2017-02-19 00:11:04 +0200

    Add/delete from binary tree

--- binary-tree.js
+++ binary-tree.js
@@ -42,9 +42,76 @@ const toArray = (node, array, index) => {
     return array;
 };
 
+const addNode = (root, val, node) => {
+    if (node === undefined) {
+        node = root;
+    }
+
+    if (node.val > val) {
+        node.left ? addNode(root, val, node.left) : (node.left = {val});
+    } else if (node.val < val) {
+        node.right ? addNode(root, val, node.right) : (node.right = {val});
+    }
+
+    return root;
+};
+
+const removeNode = (root, val, node, parent, direction) => {
+    if (node === undefined) {
+        node = root;
+    }
+
+    if (node.val === val) {
+        const oldLeft = node.left,
+            oldRight = node.right;
+
+        delete parent[direction];
+
+        /**
+        * Dirty hack
+        **/
+        const subtree = toArray(node);
+        subtree.shift();
+        subtree.sort().forEach((i) => {
+            addNode(parent, i);
+        });
+    } else if (node.val > val) {
+        removeNode(root, val, node.left, node, 'left');
+    } else if (node.val < val) {
+        removeNode(root, val, node.right, node, 'right');
+    }
+
+    return root;
+};
+
 console.log(toTree([0, 1, 2, 3, 4, 5, 6]));
 
 console.log(toArray(toTree([0, 1, 2, 3, 4, 5, 6])));
+
+const tree = toTree([0]);
+addNode(tree, 7);
+addNode(tree, 5);
+addNode(tree, 12);
+addNode(tree, -9);
+addNode(tree, -22);
+addNode(tree, -3);
+
+/**
+      0
+  -9     7
+-22 -3  5 12
+**/
+console.log(tree);
+
+removeNode(tree, -9);
+removeNode(tree, 5);
+
+/**
+      0
+  -22     7
+-3         12
+**/
+console.log(tree);
 
 module.exports = {
     toTree, toArray
