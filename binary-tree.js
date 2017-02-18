9a4ab2262c1d7c0e4b9f8f4704f8fc5d641dde1e
Kirill Maksimov <kirill.maksimov@ironsrc.com>
2017-02-18 22:38:08 +0200

    Small binary tree

---
+++ binary-tree.js
@@ -0,0 +1,47 @@
+const toTree = (array, index) => {
+    if (index === undefined) {
+        index = 0;
+    }
+
+    const val = array[index];
+
+    if (val !== undefined) {
+        return {
+            val: val,
+            left: toTree(array, 2 * index + 1),
+            right: toTree(array, 2 * index + 2)
+        };
+    }
+
+    return undefined;
+};
+
+const toArray = (node, array, index) => {
+    if (index === undefined) {
+        index = 0;
+    }
+
+    if (array === undefined) {
+        array = [];
+    }
+
+    if (node === undefined) {
+        return array;
+    } else {
+        array[index] = node.val;
+    }
+
+    if (node.left) {
+        toArray(node.left, array, 2 * index + 1);
+    }
+
+    if (node.right) {
+        toArray(node.right, array, 2 * index + 2);
+    }
+
+    return array;
+};
+
+console.log(toTree([0, 1, 2, 3, 4, 5, 6]));
+
+console.log(toArray(toTree([0, 1, 2, 3, 4, 5, 6])));
